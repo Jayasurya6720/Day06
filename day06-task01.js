829a73af46ae0fda1cbd9e9536790694f4d13849
Jayasurya6720 <jayasurya6720@gmail.com>
2023-11-24 08:44:47 +0530

    first commit

---
+++ day06-task01.js
@@ -0,0 +1,41 @@
+// Write a “person” class to hold all the details.
+
+
+// class person {
+//     constructor(name,age,gender,email,phoneNo,education,experience,location){
+//               this.name = name;
+//               this.age = age;
+//               this.gender = gender;
+//               this.education = education;
+//               this.email = email;
+//               this.phoneNo = phoneNo;
+//               this.experience = experience;
+//               this.location = location;
+//           }
+// }
+// let person01 = new person('surya',22,'bca','male','jayasurya6720@gmail.com',7373163511,'fresher','salem');
+// let person02 = new person('prasanth',20,'b.com','male','prasanth123@gmail.com',0987654321,'fresher','erode');
+// console.log(person01,person02);
+
+// output ;-
+
+// person {name: 'surya', age: 22, gender: 'bca', education: 7373163511, email: 'male', …}
+// age: 22
+// education: 7373163511
+// email: "male"
+// experience: "fresher"
+// gender: "bca"
+// location: "salem"
+// name: "surya"
+// phoneNo: "jayasurya6720@gmail.com"
+
+ 
+// person {name: 'prasanth', age: 20, gender: 'b.com', education: 987654321, email: 'male', …}
+// age: 20
+// education: 987654321
+// email: "male"
+// experience : "fresher"
+// gender : "b.com"
+// location: "erode"
+// name : "prasanth"
+// phoneNo: "prasanth123@gmail.com"
---
+++ day06-task02.js
@@ -0,0 +1,46 @@
+// 2 ) Calculate UBER Price in class 
+
+// class uber {
+//       constructor(name,km,members,time){
+//             this.name = name;
+//             this.km = km;
+//             this.members= members;
+//             this.time = time;
+// 
+//       }sum(){
+//             let kms = this.km*19;
+//             let total = this.members*kms;
+//             console.log(`Customer ID:- ${this.name}
+//              \n Total members in travel:- ${this.members} person 
+//             \n Distance at:- ${this.km} km
+//             \n Duration :- ${this.time} minutes
+//             \n Cost :- Rs ${total}`);
+//       }
+// }
+// let customer01 = new uber('riya',20,3,27);
+// let customer02 = new uber('vicky',32,2,47);
+// let cu = [customer01,customer02];
+// cu.forEach((cus)=>cus.sum());
+// 
+// Output :-
+// 
+// Customer ID:- riya
+//              
+//  Total members in travel:- 3 person 
+//             
+//  Distance at:- 20 km
+//             
+//  Duration :- 27 minutes
+//             
+//  Cost :- Rs 1140
+//  
+// 
+// Customer ID:- vicky
+//              
+//  Total members in travel:- 2 person 
+//             
+//  Distance at:- 32 km
+//             
+//  Duration :- 47 minutes
+//             
+//  Cost :- Rs 1216
